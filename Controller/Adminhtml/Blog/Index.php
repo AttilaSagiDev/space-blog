4ac432ab3e197cacc0ac00346da184a4bad0629f
Attila Sagi <attila.sagi@borngroup.com>
2023-08-11 13:30:01 +0200

    Add basic controller

---
+++ Controller/Adminhtml/Blog/Index.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright © 2023, Open Software License ("OSL") v. 3.0
+ */
+
+declare(strict_types=1);
+
+namespace Space\Blog\Controller\Adminhtml\Blog;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Backend\Model\View\Result\Page;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     */
+    const ADMIN_RESOURCE = 'Space_Blog::blog';
+
+    /**
+     * @var Registry
+     */
+    private Registry $coreRegistry;
+
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param Registry $coreRegistry
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        Registry $coreRegistry,
+        PageFactory $resultPageFactory
+    ) {
+        $this->coreRegistry = $coreRegistry;
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return ResultInterface
+     */
+    public function execute(): ResultInterface
+    {
+        die('Space_blog');
+
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Space_Blog::blog')
+            ->addBreadcrumb(__('Space'), __('Space'))
+            ->addBreadcrumb(__('Blog'), __('Blog'));
+
+        $dataPersistor = $this->_objectManager->get(DataPersistorInterface::class);
+        $dataPersistor->clear('cms_block');
+
+        return $resultPage;
+    }
+}
